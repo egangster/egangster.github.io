14b8218b342ef5aaa65b96dac3517c70de1a286a
egangster <1tapmedianerd@gmail.com>
2018-11-09 16:16:20 -0500

    testing site

---
+++ exam_proj/javascript/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	"use strict"
+	
+	$("ul").hide();
+	$(".hamburger").click(function() {
+		$("ul").slideToggle("fast", function() {
+			
+			var trigger = $("#nav"),
+			container = $("#container");
+	
+			console.log(trigger);
+			console.log(container);
+	
+			trigger.on("click", function() {
+				var $this = $(this),
+				target = $this.data("target");
+		
+				container.load(target + ".htm");
+		
+				return false;
+			});
+		});
+	});
+});
